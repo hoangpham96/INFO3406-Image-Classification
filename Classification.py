01825eccd3af8b37df5f93ae88174abf032450bb
hoangpham96 <hoangpham96@gmail.com>
2016-09-15 22:26:44 +1000

    Add Euclidean distance function and convert to grayscale function (as well as some test)

--- Classification.py
+++ Classification.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib as pl
 import pickle
+import pylab
 
 #Unpickling a file and returning its content
 def unpickle(file):
@@ -23,7 +24,7 @@ meta = unpickle(meta_file)
 label_names = meta['label_names']
 
 #Obtaining training data and lables
-training_data = []
+training_data = []					#Array of 5 training batches, each batches contain 1000 img
 training_lables = []
 
 for f in training_files:
@@ -36,5 +37,49 @@ test = unpickle(test_file)
 test_label = test['labels']
 test_data = test['data']
 
-def PCA(img):
-	
+def distance(img1,img2):
+	#Using Euclidean distance. TODO: find other more effective measures.
+	distance = (img1.astype("float")-img2.astype("float"))**2
+	distance = np.sqrt(sum(distance))
+	return distance
+
+def rgb2gray(img):
+	r, g, b = img[0:1024], img[1024:2048], img[2048:3072]
+	gray = 0.2989 * r.astype("float") + 0.5870 * g.astype("float") + 0.1140 * b.astype("float")				#Formula to turn rgb to grayscale
+	return gray
+
+
+
+
+#Below is a series of test
+#
+#Red channel of a picture with lable 1
+similar_pictures = []
+for i in range(0,len(training_data[0])):
+	if training_lables[0][i] == 1:
+		pic_red_channel = training_data[0][i][0:1024]
+		similar_pictures.append(pic_red_channel)
+
+#Red channel of a picture with different lable 
+similar_pictures2 = []
+for i in range(0,len(training_data[0])):
+	if training_lables[0][i] == 6:
+		pic_red_channel = training_data[0][i][0:1024]
+		similar_pictures2.append(pic_red_channel)
+
+
+
+# print(distance(similar_pictures[0],similar_pictures[10]))
+
+print(training_lables[0][0])
+
+pylab.figure()
+pylab.gray()
+pylab.imshow(similar_pictures2[0].reshape(32,32))
+
+
+pylab.figure()
+pylab.gray()
+pylab.imshow(rgb2gray(training_data[0][0]).reshape(32,32))
+
+pylab.show()
